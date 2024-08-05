530acfd2508dce2b4fce25da19cc6f321a194aa6
Jai28s <154686160+Jai28s@users.noreply.github.com>
2024-08-05 20:42:25 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,70 @@
+public class SudokuSolver {
+    public static void main(String[] args) {
+        int[][] unsolvedGrid = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(unsolvedGrid)) {
+            displayGrid(unsolvedGrid);
+        } else {
+            System.out.println("No solution exists for this Sudoku puzzle.");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] grid) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (grid[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValidMove(grid, row, col, num)) {
+                            grid[row][col] = num;
+                            if (solveSudoku(grid)) {
+                                return true;
+                            }
+                            grid[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false; // No valid number found
+                }
+            }
+        }
+        return true; // All cells filled
+    }
+
+    private static boolean isValidMove(int[][] grid, int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (grid[row][i] == num || grid[i][col] == num) {
+                return false; // Check row and column
+            }
+        }
+
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (grid[i][j] == num) {
+                    return false; // Check 3x3 subgrid
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static void displayGrid(int[][] grid) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                System.out.print(grid[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
